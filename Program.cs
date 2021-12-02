81049a0948e8a72c4018b6bccf0765fb07891d0f
RAYSON VINICIUS SOUZA <88290555+RaysomSousa@users.noreply.github.com>
2021-12-02 19:22:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StatusVotacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Agora digite seu ano de nascimento: ");
+            int AnoNasc = int.Parse(Console.ReadLine());
+            int AnoAtual = 2021;
+            int idade = AnoAtual - AnoNasc;
+
+            if (idade < 16)
+            {
+                Console.WriteLine("Voce ainda  não vota.");
+            }
+            else if (idade >= 16 && idade > 70)
+            {
+                Console.WriteLine("Seu voto é opcional");
+            }
+            if (idade >= 18 && idade <= 70)
+            {
+                Console.WriteLine("O seu voto é obrigatório.");
+            }
+
+            Console.WriteLine($"Sua idade é {idade}");
+            Console.ReadKey();
+        }
+    }
+    }
+
